ad2ddccc6ef6c2daa1004bb502a78446761fb608
chandramohan <chandramohan.nagaraj@gmail.com>
2020-01-13 13:54:35 +0100

    Update dblinks_report.sql

--- dblinks_report.sql
+++ dblinks_report.sql
@@ -1,10 +1,10 @@
 -- Drop existing database link 
-drop database link SKELPROD@LOC_SKELET_PROD_NEW;
+drop database link SKPRPROD@LOC_PROD_NEW;
 -- Create database link 
-create database link SKELPROD@LOC_SKELET_PROD_NEW
-  connect to SKELET
-  identified by Icon7p2
+create database link SKPRPROD@LOC_PROD_NEW
+  connect to SSSSSSSS
+  identified by XXXXX
   using '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)
   (HOST=192.168.72.100)(PORT=1521)))
-  (CONNECT_DATA=(SERVICE_NAME=SKELPROD))
+  (CONNECT_DATA=(SERVICE_NAME=SKPRPROD))
 )';
